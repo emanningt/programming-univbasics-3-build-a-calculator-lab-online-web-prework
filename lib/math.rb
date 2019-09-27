8b9e13781c102a72483ca49c72fe639ead38ae93
Eric Manning <emanningmusic@yahoo.com>
2019-09-27 21:17:49 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,6 +1,6 @@
 
 def addition(num1 = 5, num2 = 4)
-  p (num1.class + num2.class)
+  p (num1 + num2)
 end
 
 def subtraction(num1 = 10, num2 = 5)
@@ -9,17 +9,17 @@ end
 subtraction
 
 def division(num1 = 50, num2 = 2)
-  puts num1 / num2
+  p num1 / num2
 end
 division
 
 def multiplication(num1 = 4, num2 = 30)
-  puts num1 * num2
+  p num1 * num2
 end
 multiplication
 
 def modulo(num1 = 35, num2 = 4)
-
+  
 end
 
 def square_root(num)
